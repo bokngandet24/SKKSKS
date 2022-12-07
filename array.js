76554558e89c4d90271db9582a156df324c88d35
BokNgan Det <84766252+bokngandet24@users.noreply.github.com>
2022-12-07 20:32:38 +0700

    Delete array.js

--- array.js
+++
@@ -1,8 +0,0 @@
-// const luckyNumber = Math.floor(Math.random() * 99);
-
-// console.log(luckyNumber);
-
-let userInput = prompt("Enter numbper : ");
-
-if (userInput == "") console.log("No");
-else console.log(userInput);
